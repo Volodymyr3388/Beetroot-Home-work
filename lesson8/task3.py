af57779e8ce0ae40bc6baf9bbb11301b07be0609
volodymyr <volodymyrtarnavskyi33@gmail.com>
2022-11-16 16:53:12 +0200

    first commit

--- lesson8/task3.py
+++ lesson8/task3.py
@@ -14,12 +14,12 @@ def make_operations(operation, *args):
         return result
 
     elif operation == "-":
-        result = 0
-        for num in args:
+        result, *rest = args
+        for num in rest:
             result -= num
         return result
 
 
 print(make_operations("+", 5, 5, -10, -20))
 print(make_operations("*", 5, 5, -10, -20))
-print(make_operations("-", 5, 5, -10, -20)) # Я щось торможу в мене мінус якось не правильно працює і не можу зрозуміти в чому справа
+print(make_operations("-", 5, 5, -10, -20))
