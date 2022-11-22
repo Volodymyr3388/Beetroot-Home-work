85efef23f55b5addf2045fed4e6f2955f8dcf082
volodymyr <volodymyrtarnavskyi33@gmail.com>
2022-11-22 12:45:33 +0200

    first commit

--- lesson6/task2.py
+++ lesson6/task2.py
@@ -13,8 +13,9 @@ i = 0
 while i in range(10):
     a.append(randint(0, 10))
     b.append(randint(0, 10))
-    c = list(set(a) & set(b))
     i += 1
+
+c = list(set(a) & set(b))
 print(a)
 print(b)
 print(c)
